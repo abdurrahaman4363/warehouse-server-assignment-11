07bdbfb1c44fb18c340edaee0562b76068571a06
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-04-29 17:03:23 +0600

    connect to database and gitignore setpu

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // use middleware
 app.use(cors())
 app.use(express.json())
 
+// server connect to mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zj1lt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("bookStore").collection("books");
+  console.log('database connected')
+  // perform actions on the collection object
+  client.close();
+});
 
 
 // initial server setup
