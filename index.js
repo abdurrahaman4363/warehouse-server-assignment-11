2700ac0f87dbbe4f996391039d8067025ac40db3
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-04-30 15:35:38 +0600

    test server

--- index.js
+++ index.js
@@ -3,32 +3,69 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // use middleware
 app.use(cors())
 app.use(express.json())
 
-//DB_USER=warehouseUser
-//DB_PASS=@!#3iDTQP-sVwVd
 
 // server connect to mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zj1lt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("bookStore").collection("books");
-  console.log('database connected')
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('warehouse').collection('products');
+        const bookCollection = client.db('programming').collection('book');
+        const booksCollection = client.db('electronicBook').collection('books');
+
+        // to load use get method inventory item
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const inventoryItems = await cursor.toArray();
+            res.send(inventoryItems);
+        });
+       // to load use get method inventory item one
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const inventoryItem = await productCollection.findOne(query);
+            res.send(inventoryItem);
+        })
+          // to load use get method programming data
+        app.get('/programming', async (req, res) => {
+            const query = {};
+            const cursor = bookCollection.find(query);
+            const book = await cursor.toArray();
+            res.send(book);
+        });
+        // to load use get method electronic data
+        app.get('/electronic', async (req, res) => {
+            const query = {};
+            const cursor = booksCollection.find(query);
+            const books = await cursor.toArray();
+            res.send(books);
+        });
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
 
 
 // initial server setup
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('server is working')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Server is running port:', port);
 })
 
